d99318230a3bbb128b549218a4a0249c5561d3d3
Shomail <shomail1988@gmail.com>
2019-03-29 13:12:00 +0100

    search component added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
-
+import SearchParams from "./SearchParams";
 const App = () => {
   return (
     <div>
       <h1>Adopt Me Pet!</h1>
-      <Pet name="Brandy" animal="Dog" breed="Havanese" />
-      <Pet name="Cocoo" animal="Bird" breed="Parrot" />
-      <Pet name="Whisker" animal="Cat" breed="Mix" />
+      <SearchParams />
     </div>
   );
 };
