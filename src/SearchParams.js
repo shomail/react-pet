35cf5926e36d7132854d7ff958db91b1e9dc9a8c
Shomail <shomail1988@gmail.com>
2019-03-30 06:12:39 +0100

    fetching breeds from api

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,22 @@
-import React, { useState } from "react";
-import { ANIMALS } from "petfinder-client";
+import React, { useState, useEffect } from "react";
+import pf, { ANIMALS } from "petfinder-client";
 import useDropdown from "./useDropdown";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "havi", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    setBreed("");
+    petfinder.breed.list({ animal }).then(console.log, console.error);
+  }, [animal]);
 
   return (
     <div className="search-params">
