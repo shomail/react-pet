cb7b972752c09ab9b6825c16b750bf4278c4f790
Shomail <shomail1988@gmail.com>
2019-03-31 12:19:16 +0200

    404 and routing added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router } from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
+
+const FourOhFour = () => <h1>404 Not found</h1>;
+
 const App = () => {
   return (
     <React.StrictMode>
       <div>
         <h1>Adopt Me Pet!</h1>
-        <SearchParams />
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+          <FourOhFour default />
+        </Router>
       </div>
     </React.StrictMode>
   );
---
+++ src/Details.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Details = () => {
+  return <h1>Details Page</h1>;
+};
+
+export default Details;
