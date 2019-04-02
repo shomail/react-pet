775bbf632dc6a41c39c7acc74510ba68fe52228d
Shomail <shomail1988@gmail.com>
2019-04-02 14:21:50 +0200

    confirm modal added

--- src/Details.js
+++ src/Details.js
@@ -3,11 +3,12 @@ import pf from "petfinder-client";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
+import Modal from "./Modal";
 
 const petfinder = pf();
 
 class Details extends Component {
-  state = { loading: true };
+  state = { loading: true, showModal: false };
 
   async componentDidMount() {
     const data = await petfinder.pet.get({
@@ -26,10 +27,21 @@ class Details extends Component {
       loading: false
     });
   }
+
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+
   render() {
     if (this.state.loading) return <h1>Loading...</h1>;
 
-    const { animal, breed, location, description, name, media } = this.state;
+    const {
+      animal,
+      breed,
+      location,
+      description,
+      name,
+      media,
+      showModal
+    } = this.state;
 
     return (
       <div className="details">
@@ -39,13 +51,27 @@ class Details extends Component {
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
           <ThemeContext.Consumer>
             {theme => (
-              <button style={{ backgroundColor: theme[0] }}>
+              <button
+                onClick={this.toggleModal}
+                style={{ backgroundColor: theme[0] }}
+              >
                 Adopt {name}
               </button>
             )}
           </ThemeContext.Consumer>
           <p>{description}</p>
         </div>
+        {!showModal ? null : (
+          <Modal>
+            <div>
+              <h1>Would you like to adopt {name}?</h1>
+              <div className="buttons">
+                <button onClick={this.toggleModal}>Yes</button>
+                <button onClick={this.toggleModal}>No</button>
+              </div>
+            </div>
+          </Modal>
+        )}
       </div>
     );
   }
